b3539c08f60363e132e7d0e18e3db84bd90ad634
mosinskidamian11@gmail.com <mosinskidamian11@gmail.com>
2022-10-24 12:26:32 +0200

    Dodanie wymaganych kontrollerów

---
+++ src/Controller/UserController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Controller;
+
+use App\Annotation\AuthValidation;
+use App\Entity\AuthenticationToken;
+use App\Exception\DataNotFoundException;
+use App\Exception\InvalidJsonDataException;
+use App\Exception\PermissionException;
+use App\Model\AuthorizationSuccessModel;
+use App\Model\DataNotFoundModel;
+use App\Model\JsonDataInvalidModel;
+use App\Model\PermissionNotGrantedModel;
+use App\Query\AuthorizeQuery;
+use App\Repository\AuthenticationTokenRepository;
+use App\Repository\UserInformationRepository;
+use App\Repository\UserPasswordRepository;
+use App\Service\AuthorizedUserServiceInterface;
+use App\Service\RequestServiceInterface;
+use App\Tool\ResponseTool;
+use App\ValueGenerator\AuthTokenGenerator;
+use App\ValueGenerator\PasswordHashGenerator;
+use Doctrine\ORM\NonUniqueResultException;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use OpenApi\Attributes as OA;
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * AuthorizationController
+ *
+ */
+#[OA\Response(
+    response: 400,
+    description: "JSON Data Invalid",
+    content: new Model(type: JsonDataInvalidModel::class)
+)]
+#[OA\Response(
+    response: 404,
+    description: "Data not found",
+    content: new Model(type: DataNotFoundModel::class)
+)]
+#[OA\Response(
+    response: 403,
+    description: "User have no permission",
+    content: new Model(type: PermissionNotGrantedModel::class)
+)]
+
+#[OA\Tag(name: "User")]
+class UserController extends AbstractController
+{
+
+}
