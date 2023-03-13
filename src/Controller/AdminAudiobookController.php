781778693df38f2ff83ba3124d642f7103c95128
damian.mosinski@vobacom.pl <mosinskidamian11@gmail.com>
2023-03-13 16:14:09 +0100

    Poprawa errora

--- src/Controller/AdminAudiobookController.php
+++ src/Controller/AdminAudiobookController.php
@@ -1025,7 +1025,7 @@ class AdminAudiobookController extends AbstractController
 
             if ($audiobook == null) {
                 $endpointLogger->error("Audiobook dont exist");
-                throw new DataNotFoundException(["adminAudiobook.change.comment.audiobook.not.exist"]);
+                throw new DataNotFoundException(["adminAudiobook.change.cover.audiobook.not.exist"]);
             }
 
             $img = "";
@@ -1062,7 +1062,7 @@ class AdminAudiobookController extends AbstractController
 
         } else {
             $endpointLogger->error("Invalid given Query");
-            throw new InvalidJsonDataException("adminAudiobook.change.comment.cover.query");
+            throw new InvalidJsonDataException("adminAudiobook.change.cover.cover.query");
         }
     }
 }
