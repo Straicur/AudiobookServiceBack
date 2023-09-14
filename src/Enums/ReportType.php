06640f1dbedacbef60f11b3355db29c0b0e1d09c
damian.mosinski@vobacom.pl <mosinskidamian11@gmail.com>
2023-09-14 17:18:11 +0200

    Dodanie enuma typów reportów

---
+++ src/Enums/ReportType.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Enums;
+
+/**
+ * ReportType
+ */
+enum ReportType: int
+{
+    case COMMENT = 1;
+    case AUDIOBOOK_PROBLEM = 2;
+    case CATEGORY_PROBLEM = 3;
+    case SYSTEM_PROBLEM = 4;
+    case USER_PROBLEM = 5;
+    case SETTINGS_PROBLEM = 6;
+}
