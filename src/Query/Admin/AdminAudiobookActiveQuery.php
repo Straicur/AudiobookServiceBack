046d7697a6fc6d02afe80b2cf3bc08299a3f4a17
Damian Mosiński <mosinskidamian11@gmail.com>
2024-01-02 15:52:10 +0100

    feat: Dodanie powiadomienia przyd aktyuwacji audiobooka

--- src/Query/Admin/AdminAudiobookActiveQuery.php
+++ src/Query/Admin/AdminAudiobookActiveQuery.php
@@ -5,6 +5,7 @@ namespace App\Query\Admin;
 use OpenApi\Attributes as OA;
 use Symfony\Component\Uid\Uuid;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Mapping\ClassMetadata;
 
 class AdminAudiobookActiveQuery
 {
@@ -16,6 +17,40 @@ class AdminAudiobookActiveQuery
     #[Assert\NotNull(message: "Active is null")]
     #[Assert\Type(type: "boolean")]
     private bool $active;
+
+    protected array $additionalData = [];
+
+    public static function loadValidatorMetadata(ClassMetadata $metadata): void
+    {
+        $metadata->addPropertyConstraint('additionalData', new Assert\Collection([
+            'fields' => [
+                'text' => new Assert\Optional([
+                    new Assert\NotBlank(message: "Text is empty"),
+                    new Assert\NotNull(),
+                    new Assert\Type("string")
+                ])
+            ]
+        ]));
+    }
+
+    /**
+     * @param array $additionalData
+     */
+    #[OA\Property(property: "additionalData", properties: [
+        new OA\Property(property: "text", type: "string", example: "desc", nullable: true),
+    ], type: "object")]
+    public function setAdditionalData(array $additionalData): void
+    {
+        $this->additionalData = $additionalData;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getAdditionalData(): array
+    {
+        return $this->additionalData;
+    }
 
     /**
      * @return Uuid
