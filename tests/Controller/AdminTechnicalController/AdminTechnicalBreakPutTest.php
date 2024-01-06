0afdb8276f04c943b325376ec12e8e9879975beb
Damian Mosiński <mosinskidamian11@gmail.com>
2024-01-06 13:29:58 +0100

    feat: Dodanie podstawek testów

---
+++ tests/Controller/AdminTechnicalController/AdminTechnicalBreakPutTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests\Controller\AdminTechnicalController;
+
+use App\Tests\AbstractWebTest;
+
+/**
+ * AdminTechnicalBreakPutTest
+ */
+class AdminTechnicalBreakPutTest extends AbstractWebTest
+{
+
+}
