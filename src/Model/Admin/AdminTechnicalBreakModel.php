60940ab8a83b084e1c6e5ad34421a31281b33432
Straicur <mosinskidamian@gmail.com>
2024-01-06 13:28:01 +0100

    feat: Dodanie endp odpowiadającego za listę przerw

--- src/Model/Admin/AdminTechnicalBreakModel.php
+++ src/Model/Admin/AdminTechnicalBreakModel.php
@@ -4,5 +4,74 @@ namespace App\Model\Admin;
 
 class AdminTechnicalBreakModel
 {
+    public string $id;
+    public bool $active;
+    public int $dateFrom;
+    public ?int $dateTo = null;
+    public string $user;
+
+    /**
+     * @param string $id
+     * @param bool $active
+     * @param int $dateFrom
+     * @param int $user
+     */
+    public function __construct(string $id, bool $active, \DateTime $dateFrom, string $user)
+    {
+        $this->id = $id;
+        $this->active = $active;
+        $this->dateFrom = $dateFrom->getTimestamp();
+        $this->user = $user;
+    }
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    public function setId(string $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function isActive(): bool
+    {
+        return $this->active;
+    }
+
+    public function setActive(bool $active): void
+    {
+        $this->active = $active;
+    }
+
+    public function getDateFrom(): int
+    {
+        return $this->dateFrom;
+    }
+
+    public function setDateFrom(\DateTime $dateFrom): void
+    {
+        $this->dateFrom = $dateFrom->getTimestamp();
+    }
+
+    public function getDateTo(): ?int
+    {
+        return $this->dateTo;
+    }
+
+    public function setDateTo(\DateTime $dateTo): void
+    {
+        $this->dateTo = $dateTo->getTimestamp();
+    }
+
+    public function getUser(): string
+    {
+        return $this->user;
+    }
+
+    public function setUser(string $user): void
+    {
+        $this->user = $user;
+    }
 
 }
--- src/Query/Admin/AdminTechnicalBreakListQuery.php
+++ src/Query/Admin/AdminTechnicalBreakListQuery.php
@@ -2,14 +2,129 @@
 
 namespace App\Query\Admin;
 
+use App\Enums\TechnicalBreakOrder;
 use OpenApi\Attributes as OA;
 use Symfony\Component\Uid\Uuid;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Mapping\ClassMetadata;
 
 class AdminTechnicalBreakListQuery
 {
-    #[Assert\NotNull(message: "ReportId is null")]
-    #[Assert\NotBlank(message: "ReportId is blank")]
-    #[Assert\Uuid]
-    private Uuid $reportId;
+    #[Assert\NotNull(message: "Page is null")]
+    #[Assert\NotBlank(message: "Page is empty")]
+    #[Assert\Type(type: "integer")]
+    private int $page;
+
+    #[Assert\NotNull(message: "Limit is null")]
+    #[Assert\NotBlank(message: "Limit is empty")]
+    #[Assert\Type(type: "integer")]
+    private int $limit;
+
+    protected array $searchData = [];
+
+    public static function loadValidatorMetadata(ClassMetadata $metadata): void
+    {
+        $metadata->addPropertyConstraint('searchData', new Assert\Collection([
+            'fields' => [
+                'userId' => new Assert\Optional([
+                    new Assert\NotBlank(),
+                    new Assert\Regex(pattern: '/^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/', message: 'Bad Uuid'),
+                    new Assert\Uuid(),
+                ]),
+                'active' => new Assert\Optional([
+                    new Assert\NotBlank(),
+                    new Assert\Type(type: 'boolean', message: 'The value {{ value }} is not a valid {{ type }}'),
+                ]),
+                'order' => new Assert\Optional([
+                    new Assert\NotBlank(message: 'Order is empty'),
+                    new Assert\Type(type: 'integer', message: 'The value {{ value }} is not a valid {{ type }}'),
+                    new Assert\GreaterThan(0),
+                    new Assert\LessThan(9)
+                ]),
+                'dateFrom' => new Assert\Optional([
+                    new Assert\NotBlank(message: 'DateFrom is empty'),
+                    new Assert\Type(type: 'datetime', message: 'The value {{ value }} is not a valid {{ type }}'),
+                ]),
+                'dateTo' => new Assert\Optional([
+                    new Assert\NotBlank(message: 'DateTo is empty'),
+                    new Assert\Type(type: 'datetime', message: 'The value {{ value }} is not a valid {{ type }}'),
+                ]),
+            ],
+        ]));
+    }
+
+    /**
+     * @param string[] $searchData
+     */
+    #[OA\Property(property: 'searchData', properties: [
+        new OA\Property(property: 'userId', type: 'string', example: 'UUID', nullable: true),
+        new OA\Property(property: 'active', type: 'boolean', example: true, nullable: true),
+        new OA\Property(property: 'order', type: 'integer', example: 1, nullable: true),
+        new OA\Property(property: 'dateFrom', type: 'datetime', example: 'd.m.Y', nullable: true),
+        new OA\Property(property: 'dateTo', type: 'datetime', example: 'd.m.Y', nullable: true),
+    ], type: 'object')]
+    public function setSearchData(array $searchData): void
+    {
+        if (array_key_exists('userId', $searchData) && Uuid::isValid($searchData["userId"])) {
+            $searchData["userId"] = Uuid::fromString($searchData["userId"]);
+        }
+
+        if (array_key_exists('order', $searchData)) {
+            if ($searchData["order"] != TechnicalBreakOrder::LATEST->value && $searchData["order"] != TechnicalBreakOrder::OLDEST->value && $searchData["order"] != TechnicalBreakOrder::ACTIVE->value) {
+                $searchData["order"] = TechnicalBreakOrder::ACTIVE->value;
+            }
+        }
+
+        if (array_key_exists('dateFrom', $searchData)) {
+            $searchData['dateFrom'] = \DateTime::createFromFormat('d.m.Y', $searchData['dateFrom']);
+        }
+        if (array_key_exists('dateTo', $searchData)) {
+            $searchData['dateTo'] = \DateTime::createFromFormat('d.m.Y', $searchData['dateTo']);
+        }
+
+        $this->searchData = $searchData;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getSearchData(): array
+    {
+        return $this->searchData;
+    }
+
+    /**
+     * @return int
+     */
+    #[OA\Property(type: "integer", example: 0)]
+    public function getPage(): int
+    {
+        return $this->page;
+    }
+
+    /**
+     * @param int $page
+     */
+    public function setPage(int $page): void
+    {
+        $this->page = $page;
+    }
+
+    /**
+     * @return int
+     */
+    #[OA\Property(type: "integer", example: 10)]
+    public function getLimit(): int
+    {
+        return $this->limit;
+    }
+
+    /**
+     * @param int $limit
+     */
+    public function setLimit(int $limit): void
+    {
+        $this->limit = $limit;
+    }
+
 }
