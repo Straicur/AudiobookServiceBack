afdaac4ccfaa3d970cd43f2ba09737937d8be209
Damian Mosiński <mosinskidamian11@gmail.com>
2024-01-06 13:28:01 +0100

    feat: Dodanie endp odpowiadającego za listę przerw

--- src/Repository/TechnicalBreakRepository.php
+++ src/Repository/TechnicalBreakRepository.php
@@ -3,10 +3,10 @@
 namespace App\Repository;
 
 use App\Entity\TechnicalBreak;
+use App\Enums\TechnicalBreakOrder;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\ORM\Exception\ORMException;
-use Doctrine\ORM\OptimisticLockException;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Uid\Uuid;
 
 /**
  * @extends ServiceEntityRepository<TechnicalBreak>
@@ -49,6 +49,62 @@ class TechnicalBreakRepository extends ServiceEntityRepository
         }
     }
 
+    /**
+     * @param Uuid|null $userId
+     * @param bool|null $active
+     * @param int|null $order
+     * @param \DateTime|null $dateFrom
+     * @param \DateTime|null $dateTo
+     * @return TechnicalBreak[]
+     */
+    public function getTechnicalBreakByPage(?Uuid $userId, ?bool $active, ?int $order, ?\DateTime $dateFrom, ?\DateTime $dateTo): array
+    {
+        $qb = $this->createQueryBuilder('tb');
+
+        if ($userId !== null) {
+            $qb->andWhere('tb.user = :userId')
+                ->setParameter('userId', $userId->toBinary());
+        }
+
+        if ($active !== null) {
+            $qb->andWhere('tb.active = :active')
+                ->setParameter('active', $active);
+        }
+
+        if ($dateFrom !== null && $dateTo !== null) {
+            $qb->andWhere('((tb.dateFrom > :dateFrom) AND (tb.dateFrom < :dateTo))')
+                ->setParameter('dateFrom', $dateFrom)
+                ->setParameter('dateTo', $dateTo);
+        } else if ($dateTo != null) {
+            $qb->andWhere('(tb.dateFrom < :dateTo)')
+                ->setParameter('dateTo', $dateTo);
+        } else if ($dateFrom != null) {
+            $qb->andWhere('(tb.dateFrom > :dateFrom)')
+                ->setParameter('dateFrom', $dateFrom);
+        }
+
+        if ($order !== null) {
+            switch ($order) {
+                case TechnicalBreakOrder::LATEST->value:
+                {
+                    $qb->orderBy("a.dateAdd", "DESC");
+                    break;
+                }
+                case TechnicalBreakOrder::OLDEST->value:
+                {
+                    $qb->orderBy("a.dateAdd", "ASC");
+                    break;
+                }
+                case TechnicalBreakOrder::ACTIVE->value:
+                {
+                    $qb->orderBy("tb.active", "DESC");
+                    break;
+                }
+            }
+        }
+
+        return $qb->getQuery()->execute();
+    }
 //    /**
 //     * @return TechnicalBreak[] Returns an array of TechnicalBreak objects
 //     */
