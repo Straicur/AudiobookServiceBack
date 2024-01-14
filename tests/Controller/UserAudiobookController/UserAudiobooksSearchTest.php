b0653fcbd60a5a64f77ee1f44e0f5dd554701717
Straicur <mosinskidamian@gmail.com>
2024-01-14 08:50:28 +0100

    fix: dodanie brakujących sprawedzeń działania kontroli rodzicielskiej

--- tests/Controller/UserAudiobookController/UserAudiobooksSearchTest.php
+++ tests/Controller/UserAudiobookController/UserAudiobooksSearchTest.php
@@ -65,6 +65,59 @@ class UserAudiobooksSearchTest extends AbstractWebTest
 
     /**
      * step 1 - Preparing data
+     * step 2 - Sending Request
+     * step 3 - Checking response
+     * step 4 - Checking response has returned correct data
+     * @return void
+     */
+    public function test_userAudiobooksSearchParentControlCorrect(): void
+    {
+        /// step 1
+        $user = $this->databaseMockManager->testFunc_addUser("User", "Test", "test@cos.pl", "+48123123123", ["Guest", "User", "Administrator"], true, "zaq12wsx", birthday: (new \DateTime('Now'))->modify('-14 year'));
+
+        $category1 = $this->databaseMockManager->testFunc_addAudiobookCategory("1");
+        $category2 = $this->databaseMockManager->testFunc_addAudiobookCategory("2", $category1);
+        $category3 = $this->databaseMockManager->testFunc_addAudiobookCategory("3", $category1);
+        $category4 = $this->databaseMockManager->testFunc_addAudiobookCategory("4", $category3);
+        $category5 = $this->databaseMockManager->testFunc_addAudiobookCategory("5", $category2, true);
+
+        $audiobook1 = $this->databaseMockManager->testFunc_addAudiobook("tt1", "a", "2", "d", new \DateTime("Now"), "20", "20", 2, "desc", AudiobookAgeRange::FROM3TO7, "d1", [$category1, $category2], active: true);
+        $audiobook2 = $this->databaseMockManager->testFunc_addAudiobook("a2", "a", "2", "d", new \DateTime("Now"), "20", "20", 2, "desc", AudiobookAgeRange::ABOVE18, "d2", [$category2], active: true);
+        $audiobook3 = $this->databaseMockManager->testFunc_addAudiobook("dt3", "a", "2", "d", new \DateTime("Now"), "20", "20", 2, "desc", AudiobookAgeRange::FROM7TO12, "d3", [$category2], active: true);
+        $audiobook4 = $this->databaseMockManager->testFunc_addAudiobook("b4", "a", "2", "d", new \DateTime("Now"), "20", "20", 2, "desc", AudiobookAgeRange::ABOVE18, "d4", [$category1, $category2], active: true);
+        $audiobook5 = $this->databaseMockManager->testFunc_addAudiobook("t5", "a", "2", "d", new \DateTime("Now"), "20", "20", 2, "desc", AudiobookAgeRange::FROM12TO16, "d5", [$category2], active: true);
+        $audiobook6 = $this->databaseMockManager->testFunc_addAudiobook("n6", "trhrthr", "2", "d", new \DateTime("Now"), "20", "20", 2, "desc", AudiobookAgeRange::FROM12TO16, "d6", [$category5]);
+        $audiobook7 = $this->databaseMockManager->testFunc_addAudiobook("m7", "trtr", "2", "d", new \DateTime("Now"), "20", "20", 2, "desc", AudiobookAgeRange::FROM16TO18, "d7", [$category1, $category4], active: true);
+        $audiobook8 = $this->databaseMockManager->testFunc_addAudiobook("j8", "a", "2", "d", new \DateTime("Now"), "20", "20", 2, "desc", AudiobookAgeRange::FROM16TO18, "d8", [$category4]);
+        $audiobook9 = $this->databaseMockManager->testFunc_addAudiobook("aaat9", "a", "2", "d", new \DateTime("Now"), "20", "20", 2, "desc", AudiobookAgeRange::ABOVE18, "d9", [$category4], active: true);
+
+        $token = $this->databaseMockManager->testFunc_loginUser($user);
+
+        $content = [
+            "title" => "t"
+        ];
+
+        /// step 2
+        $crawler = self::$webClient->request("POST", "/api/user/audiobooks/search", server: [
+            "HTTP_authorization" => $token->getToken()
+        ], content: json_encode($content));
+
+        /// step 3
+        self::assertResponseIsSuccessful();
+        self::assertResponseStatusCodeSame(200);
+
+        $response = self::$webClient->getResponse();
+
+        $responseContent = json_decode($response->getContent(), true);
+        /// step 5
+        $this->assertIsArray($responseContent);
+
+        $this->assertArrayHasKey("audiobooks", $responseContent);
+        $this->assertCount(3, $responseContent["audiobooks"]);
+    }
+
+    /**
+     * step 1 - Preparing data
      * step 2 - Sending Request without content
      * step 3 - Checking response
      *
