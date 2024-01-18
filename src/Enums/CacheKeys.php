7855e111bbf6ff352ea3b34dc96c7413360418c7
Damian Mosiński <mosinskidamian11@gmail.com>
2024-01-18 18:26:39 +0100

    feat: Dalsza implementacja cache oraz poprawki kodu

--- src/Enums/CacheKeys.php
+++ src/Enums/CacheKeys.php
@@ -13,7 +13,8 @@ enum CacheKeys: string
     case ADMIN_CATEGORY_AUDIOBOOKS = "AdminCategoryAudiobooks";
     case ADMIN_AUDIOBOOK = "AdminAudiobook";
     case ADMIN_AUDIOBOOK_COMMENTS = "AdminAudiobookComments";
-    case ADMIN_REPORTS = "AdminReports";
-    case ADMIN_REPORT = "AdminReport";
     case ADMIN_STATISTICS = "AdminStatistics";
+    case ADMIN_STATISTICS_AUDIOBOOKS = "AdminStatisticsAudiobooks";
+    case ADMIN_ROLES = "AdminRoles";
+    case USER_AUDIOBOOK_PART = "UserAudiobookPart";
 }
