8564ef4c000ed1499ccf265266d357241b3c92fd
Damian Mosiński <mosinskidamian11@gmail.com>
2024-01-18 18:37:00 +0100

    feat: Obsłużenie błędu cache w Subscriberze

--- src/EventSubscriber/ExceptionSubscriber.php
+++ src/EventSubscriber/ExceptionSubscriber.php
@@ -2,8 +2,10 @@
 
 namespace App\EventSubscriber;
 
+use App\Exception\CacheException;
 use App\Exception\DataNotFoundException;
 use App\Exception\ResponseExceptionInterface;
+use Psr\Cache\InvalidArgumentException;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
@@ -46,11 +48,25 @@ class ExceptionSubscriber implements EventSubscriberInterface
         } else {
             $this->responseLogger->critical("ResponseException", ["class" => $exception::class, "data" => $exception]);
 
+            $loggingContext = [
+                "message" => $exception->getMessage(),
+                "file" => "[" . $exception->getLine() . "](" . $exception->getFile() . ")",
+            ];
+
             switch ($exception::class) {
                 case NotFoundHttpException::class:
                 {
                     $notFoundException = new DataNotFoundException([$exception->getMessage()]);
 
+                    $this->responseLogger->info("NotFoundHttpException", $loggingContext);
+                    $event->setResponse($notFoundException->getResponse());
+                    break;
+                }
+                case InvalidArgumentException::class:
+                {
+                    $notFoundException = new CacheException();
+
+                    $this->responseLogger->info("InvalidArgumentException", $loggingContext);
                     $event->setResponse($notFoundException->getResponse());
                     break;
                 }
