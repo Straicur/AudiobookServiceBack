5e4d3f4b7572684dfa32af88172278ca70dd4f59
Straicur <mosinskidamian@gmail.com>
2024-01-20 12:02:08 +0100

    feat: Dodanie działania endpointów od cache

---
+++ src/Model/Admin/CacheModel.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Model\Admin;
+
+use App\Model\Error\ModelInterface;
+
+class CacheModel implements ModelInterface
+{
+    private string $value;
+
+    /**
+     * @param string $value
+     */
+    public function __construct(string $value)
+    {
+        $this->value = $value;
+    }
+
+    public function getValue(): string
+    {
+        return $this->value;
+    }
+
+    public function setValue(string $value): void
+    {
+        $this->value = $value;
+    }
+
+}
