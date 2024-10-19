d39514403405f89001802789818b421002a263be
mosinskidamian11@gmail.com <mosinskidamian11@gmail.com>
2024-10-19 16:38:39 +0200

    fix: Poprawki po testach

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -4,32 +4,21 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use App\Entity\MyList;
-use App\Entity\ProposedAudiobooks;
 use App\Entity\RegisterCode;
-use App\Entity\User;
-use App\Entity\UserInformation;
-use App\Entity\UserPassword;
-use App\Entity\UserSettings;
 use App\Exception\DataNotFoundException;
 use App\Exception\InvalidJsonDataException;
 use App\Model\Error\DataNotFoundModel;
 use App\Model\Error\JsonDataInvalidModel;
 use App\Query\Common\RegisterConfirmSendQuery;
 use App\Query\Common\RegisterQuery;
-use App\Repository\InstitutionRepository;
-use App\Repository\MyListRepository;
-use App\Repository\ProposedAudiobooksRepository;
 use App\Repository\RegisterCodeRepository;
 use App\Repository\RoleRepository;
 use App\Repository\UserInformationRepository;
-use App\Repository\UserPasswordRepository;
 use App\Repository\UserRepository;
-use App\Repository\UserSettingsRepository;
 use App\Service\RequestServiceInterface;
 use App\Service\TranslateService;
+use App\Service\User\UserRegisterService;
 use App\Tool\ResponseTool;
-use App\ValueGenerator\PasswordHashGenerator;
 use App\ValueGenerator\RegisterCodeGenerator;
 use DateTime;
 use Nelmio\ApiDocBundle\Annotation\Model;
@@ -76,122 +65,21 @@ class RegisterController extends AbstractController
     public function register(
         Request $request,
         RequestServiceInterface $requestServiceInterface,
-        UserInformationRepository $userInformationRepository,
-        UserRepository $userRepository,
         LoggerInterface $endpointLogger,
         LoggerInterface $usersLogger,
-        RegisterCodeRepository $registerCodeRepository,
-        MailerInterface $mailer,
-        RoleRepository $roleRepository,
-        MyListRepository $myListRepository,
-        ProposedAudiobooksRepository $proposedAudiobooksRepository,
-        InstitutionRepository $institutionRepository,
-        UserPasswordRepository $userPasswordRepository,
         TranslateService $translateService,
-        UserSettingsRepository $userSettingsRepository,
+        UserRegisterService $registerService,
     ): Response {
         $registerQuery = $requestServiceInterface->getRequestBodyContent($request, RegisterQuery::class);
 
         if ($registerQuery instanceof RegisterQuery) {
-            $existingEmail = $userInformationRepository->findOneBy([
-                'email' => $registerQuery->getEmail(),
-            ]);
+            $registerService->checkExistingUsers($registerQuery, $request);
+            $registerService->checkInstitutionLimits($request);
 
-            if ($existingEmail !== null) {
-                $endpointLogger->error('Email already exists');
-                $translateService->setPreferredLanguage($request);
-                throw new DataNotFoundException([$translateService->getTranslation('EmailExists')]);
-            }
+            $newUser = $registerService->createUser($registerQuery);
 
-            $existingPhone = $userInformationRepository->findOneBy([
-                'phoneNumber' => $registerQuery->getPhoneNumber(),
-            ]);
-
-            if ($existingPhone !== null) {
-                $endpointLogger->error('Phone number already exists');
-                $translateService->setPreferredLanguage($request);
-                throw new DataNotFoundException([$translateService->getTranslation('PhoneNumberExists')]);
-            }
-
-            $institution = $institutionRepository->findOneBy([
-                'name' => $_ENV['INSTITUTION_NAME'],
-            ]);
-
-            $guest = $roleRepository->findOneBy([
-                'name' => 'Guest',
-            ]);
-
-            if ($institution->getMaxUsers() < count($userRepository->getUsersByRole($guest))) {
-                $endpointLogger->error('Too much users');
-                $translateService->setPreferredLanguage($request);
-                throw new DataNotFoundException([$translateService->getTranslation('ToMuchUsers')]);
-            }
-
-            $newUser = new User();
-
-            $userRepository->add($newUser, false);
-
-            $additionalData = $registerQuery->getAdditionalData();
-
-            $newUserInformation = new UserInformation(
-                $newUser,
-                $registerQuery->getEmail(),
-                $registerQuery->getPhoneNumber(),
-                $registerQuery->getFirstname(),
-                $registerQuery->getLastname(),
-            );
-
-            $birthday = $additionalData['birthday'] ?? null;
-
-            if ($birthday !== null) {
-                $newUserInformation->setBirthday($birthday);
-            }
-
-            $newUser->setUserInformation($newUserInformation);
-
-            $userSettingsRepository->add(new UserSettings($newUser));
-
-            $userMyList = new MyList($newUser);
-
-            $myListRepository->add($userMyList);
-
-            $userProposedAudiobooks = new ProposedAudiobooks($newUser);
-
-            $proposedAudiobooksRepository->add($userProposedAudiobooks);
-
-            $userRole = $roleRepository->findOneBy([
-                'name' => 'Guest',
-            ]);
-
-            $newUser->addRole($userRole);
-
-            $passwordGenerator = new PasswordHashGenerator($registerQuery->getPassword());
-
-            $userPasswordEntity = new UserPassword($newUser, $passwordGenerator);
-
-            $userPasswordRepository->add($userPasswordEntity);
-
-            $registerCodeGenerator = new RegisterCodeGenerator();
-
-            $registerCode = new RegisterCode($registerCodeGenerator, $newUser);
-
-            $registerCodeRepository->add($registerCode);
-
-            if ($_ENV['APP_ENV'] !== 'test') {
-                $email = (new TemplatedEmail())
-                    ->from($_ENV['INSTITUTION_EMAIL'])
-                    ->to($newUser->getUserInformation()->getEmail())
-                    ->subject($translateService->getTranslation('AccountActivationCodeSubject'))
-                    ->htmlTemplate('emails/register.html.twig')
-                    ->context([
-                        'userName'  => $newUser->getUserInformation()->getFirstname() . ' ' . $newUser->getUserInformation()->getLastname(),
-                        'code'      => $registerCodeGenerator->getBeforeGenerate(),
-                        'userEmail' => $newUser->getUserInformation()->getEmail(),
-                        'url'       => $_ENV['BACKEND_URL'],
-                        'lang'      => $request->getPreferredLanguage() !== null ? $request->getPreferredLanguage() : $translateService->getLocate(),
-                    ]);
-                $mailer->send($email);
-            }
+            $registerCode = $registerService->getRegisterCode($newUser);
+            $registerService->sendMail($newUser, $registerCode, $request);
 
             $usersLogger->info('user.' . $newUser->getUserInformation()->getEmail() . 'registered');
             return ResponseTool::getResponse(httpCode: 201);
